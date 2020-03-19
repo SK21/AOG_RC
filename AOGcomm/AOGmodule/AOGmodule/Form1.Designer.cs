62592ccbe427a0ef70da044db9a15feb5bfdd94f
SK21 <SK21@users.noreply.github.com>
2020-03-19 12:20:05 -0600

    Testing AgOpenGPS comm

---
+++ AOGcomm/AOGmodule/AOGmodule/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace AOGmodule
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbReceive = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tbServerIP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbReceive
+            // 
+            this.tbReceive.Location = new System.Drawing.Point(11, 182);
+            this.tbReceive.Multiline = true;
+            this.tbReceive.Name = "tbReceive";
+            this.tbReceive.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbReceive.Size = new System.Drawing.Size(293, 112);
+            this.tbReceive.TabIndex = 8;
+            this.tbReceive.TextChanged += new System.EventHandler(this.tbReceive_TextChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Receive:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 26);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Send test PGN31100 (for AOG)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(270, 26);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Send test PGN33000 (for companion apps)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 53);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(270, 26);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Send test PGN32100 (for modules)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tbServerIP
+            // 
+            this.tbServerIP.Location = new System.Drawing.Point(82, 129);
+            this.tbServerIP.Name = "tbServerIP";
+            this.tbServerIP.Size = new System.Drawing.Size(110, 20);
+            this.tbServerIP.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Server IP:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 308);
+            this.Controls.Add(this.tbServerIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbReceive);
+            this.Name = "Form1";
+            this.Text = "AOG Module";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbReceive;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox tbServerIP;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AOGcomm/AOGmodule/AOGmodule/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AOGmodule
+{
+    public partial class Form1 : Form
+    {
+        private UDPcomm UDPobj;
+
+        byte[] Data = new byte[10];
+
+        public Form1()
+        {
+            InitializeComponent();
+            tbServerIP.Text = "192.168.2.255";
+            UDPobj = new UDPcomm(this, tbServerIP.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UDPobj.Start();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Data[0] = 121;
+            Data[1] = 124;
+            UDPobj.SendMessage(Data);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Data[0] = 125;
+            Data[1] = 100;
+            UDPobj.SendMessage(Data);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Data[0] = 128;
+            Data[1] = 232;
+            UDPobj.SendMessage(Data);
+        }
+
+        private void tbReceive_TextChanged_1(object sender, EventArgs e)
+        {
+            tbReceive.SelectionStart = tbReceive.Text.Length;
+            tbReceive.ScrollToCaret();
+        }
+    }
+}
