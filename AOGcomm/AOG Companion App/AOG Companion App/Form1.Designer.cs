97656c9fbb0b79f8df8bc162d8a784c00b18c9ec
SK21 <SK21@users.noreply.github.com>
2020-12-05 18:06:16 -0600

    example comm

---
+++ AOGcomm/AOG Companion App/AOG Companion App/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace AOG_Companion_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbReceive = new System.Windows.Forms.TextBox();
+            this.tbSendPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tbReceivePort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(270, 26);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Send test PGN32200 (for modules)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(270, 26);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Send test PGN33500 (for companion apps)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 26);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Send test PGN31200 (for AOG)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Event Log:";
+            // 
+            // tbReceive
+            // 
+            this.tbReceive.Location = new System.Drawing.Point(11, 224);
+            this.tbReceive.Multiline = true;
+            this.tbReceive.Name = "tbReceive";
+            this.tbReceive.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbReceive.Size = new System.Drawing.Size(270, 112);
+            this.tbReceive.TabIndex = 15;
+            this.tbReceive.TextChanged += new System.EventHandler(this.tbReceive_TextChanged);
+            // 
+            // tbSendPort
+            // 
+            this.tbSendPort.Location = new System.Drawing.Point(142, 38);
+            this.tbSendPort.Name = "tbSendPort";
+            this.tbSendPort.Size = new System.Drawing.Size(110, 20);
+            this.tbSendPort.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Send Port (any)";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(270, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 46);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "Start";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // tbReceivePort
+            // 
+            this.tbReceivePort.Location = new System.Drawing.Point(142, 12);
+            this.tbReceivePort.Name = "tbReceivePort";
+            this.tbReceivePort.Size = new System.Drawing.Size(110, 20);
+            this.tbReceivePort.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Receive Port (8888-8892)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 355);
+            this.Controls.Add(this.tbReceivePort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.tbSendPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbReceive);
+            this.Name = "Form1";
+            this.Text = "AOG Companion App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbReceive;
+        private System.Windows.Forms.TextBox tbSendPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox tbReceivePort;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
