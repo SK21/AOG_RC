2f014f7458d5f62a55e5b8ac8987448c08731e92
SK21 <SK21@users.noreply.github.com>
2023-06-11 17:43:19 -0600

    update teensy rate, comm

--- Modules/Teensy Rate/RCteensy/Rate.ino
+++ Modules/Teensy Rate/RCteensy/Rate.ino
@@ -39,52 +39,41 @@ void ISR0()
 		dur = 0xFFFFFFFF + micronow - PulseTime;
 	}
 
-	if (dur > 1000000)
+	if (dur > DurOff)
 	{
 		// the component was off so reset the values
 		avDurs[0] = 0;
 		dur = 50000;
-		for (int i = 0; i < (avgPulses - 1); i++)
+		for (int i = 0; i < avgPulses; i++)
 		{
 			Durations[0][i] = 0;
 		}
+		DurCount[0] = 0;
+		FullCount[0] = false;
+
 		PulseTime = micronow;
 		PulseCount[0]++;
-		FullCount[0] = false;
 	}
 	else if (dur > Sensor[0].Debounce * 1000)
 	{
-		PulseCount[0]++;
+		if (avDurs[0] == 0) avDurs[0] = dur;
+
 		// check to see if the dur value is too long like an interrupt was missed.
-		if ((dur > (1.5 * avDurs[0])) && (avDurs[0] != 0))
+		if (dur > (2.5 * avDurs[0])) dur = avDurs[0];
+
+		Duration[0] = dur;
+		Durations[0][DurCount[0]] = dur;
+
+		if (DurCount[0] > 0) avDurs[0] = ((Durations[0][DurCount[0] - 1]) + dur) / 2;
+
+		if (DurCount[0]++ >= avgPulses)
 		{
-			Durations[0][DurCount[0]] = avDurs[0];
-			Duration[0] = avDurs[0];
-			dur = avDurs[0];
-		}
-		else
-		{
-			Durations[0][DurCount[0]] = dur;
-			Duration[0] = dur;
+			DurCount[0] = 0;
+			FullCount[0] = true;
 		}
 
 		PulseTime = micronow;
-		if (DurCount[0] == 0)
-		{
-			DurCount[0]++;
-			avDurs[0] = (Durations[0][avgPulses - 1] + dur) / 2;
-		}
-		else if (DurCount[0] < avgPulses)
-		{
-			DurCount[0]++;
-			avDurs[0] = (Durations[0][DurCount[0] - 1] + dur) / 2;
-		}
-		else
-		{
-			DurCount[0] = 0;
-			avDurs[0] = (Durations[0][DurCount[0] - 1] + dur) / 2;
-			FullCount[0] = true;
-		}
+		PulseCount[0]++;
 	}
 }
 
@@ -200,7 +189,7 @@ void GetUPMflow(int ID)
 			// low ms/pulse, use pulses over time
 			if (FullCount[ID])
 			{
-				PPM[ID] = 60000000 / GetAvgDuration(ID);
+				PPM[ID] = 6000000000 / GetAvgDuration(ID);
 			}
 			else
 			{
@@ -216,25 +205,15 @@ void GetUPMflow(int ID)
 			}
 			else
 			{
-				unsigned long tmp = 6000000000 / CurrentDuration;
-
-				// olympic average
-				Osum[ID] += tmp;
-				if (Omax[ID] < tmp) Omax[ID] = tmp;
-				if (Omin[ID] > tmp) Omin[ID] = tmp;
-
-				Ocount[ID]++;
-				if (Ocount[ID] > 4)
+				// high ms/pulse, use time for one pulse
+				if (CurrentDuration == 0)
 				{
-					Osum[ID] -= Omax[ID];
-					Osum[ID] -= Omin[ID];
-					Oave[ID] = (float)Osum[ID] / 300.0;	// divide by 3 samples and divide by 100 for decimal place
-					Osum[ID] = 0;
-					Omax[ID] = 0;
-					Omin[ID] = 5000000000;
-					Ocount[ID] = 0;
+					PPM[ID] = 0;
 				}
-				PPM[ID] = Oave[ID];
+				else
+				{
+					PPM[ID] = 6000000000 / CurrentDuration;
+				}
 			}
 		}
 
@@ -251,10 +230,27 @@ void GetUPMflow(int ID)
 		Ocount[ID] = 0;
 	}
 
+	// olympic average
+	Osum[ID] += PPM[ID];
+	if (Omax[ID] < PPM[ID]) Omax[ID] = PPM[ID];
+	if (Omin[ID] > PPM[ID]) Omin[ID] = PPM[ID];
+
+	Ocount[ID]++;
+	if (Ocount[ID] > 4)
+	{
+		Osum[ID] -= Omax[ID];
+		Osum[ID] -= Omin[ID];
+		Oave[ID] = (float)Osum[ID] / 300.0;	// divide by 3 samples and divide by 100 for decimal place
+		Osum[ID] = 0;
+		Omax[ID] = 0;
+		Omin[ID] = 5000000000;
+		Ocount[ID] = 0;
+	}
+
 	// units per minute
 	if (Sensor[ID].MeterCal > 0)
 	{
-		Sensor[ID].UPM = (float)PPM[ID] / (float)Sensor[ID].MeterCal;
+		Sensor[ID].UPM = (float)Oave[ID] / (float)Sensor[ID].MeterCal;
 	}
 	else
 	{
