741783a612ae9147d4b99a43a6d2161c511385da
SK21 <SK21@users.noreply.github.com>
2024-04-27 12:52:39 -0600

    fix pid and send comm for nano rate

--- Modules/Nano/RCnano/PID.ino
+++ Modules/Nano/RCnano/PID.ino
@@ -148,7 +148,7 @@ int PIDvalve(byte ID)
 				IntegralSum[ID] *= (Sensor[ID].KI > 0);	// zero out if not using KI
 
 				DifValue = Sensor[ID].KD * (LastUPM[ID] - Sensor[ID].UPM) * 10.0;
-				Result = Sensor[ID].MinPWM + Sensor[ID].KP * SF * RateError + IntegralSum[ID] + DifValue;
+				Result = Sensor[ID].KP * SF * RateError + IntegralSum[ID] + DifValue;
 
 				bool IsPositive = (Result > 0);
 				Result = abs(Result);
